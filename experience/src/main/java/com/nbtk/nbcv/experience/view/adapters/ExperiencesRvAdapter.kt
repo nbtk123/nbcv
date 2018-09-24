0c8f5e1c75d77ba422cbf1a852fb72f1fbb50b60
Nir Barzilay <nbtk123@gmail.com>
2018-09-24 13:17:12 +0300

    added adapter for the timeline recyclerview

---
+++ experience/src/main/java/com/nbtk/nbcv/experience/view/adapters/ExperiencesRvAdapter.kt
@@ -0,0 +1,20 @@
+package com.nbtk.nbcv.experience.view.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ExperiencesRvAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
